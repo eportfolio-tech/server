93cb2abe676275bca9362cf5cc35fbc817af61b4
Haswell <shuyangf@student.unimelb.edu.au>
2020-08-26 03:29:27 +1000

    feat: Implement User Management API

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM openjdk:12-alpine
+#VOLUME /tmp
+#ENV JAVA_OPTS="-Xdebug"
+RUN apk --no-cache add curl
+RUN addgroup -S spring && adduser -S spring -G spring
+USER spring:spring
+ARG JAR_FILE=target/*.jar
+COPY ${JAR_FILE} app.jar
+ENTRYPOINT ["java", "-jar","/app.jar"]
---
+++ src/main/java/tech/eportfolio/server/User.java
@@ -0,0 +1,33 @@
+package tech.eportfolio.server;
+
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long id;
+    private String firstName;
+    private String lastName;
+
+    @Column(nullable = false)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+
+    private String preferedName;
+    private String title;
+    private String phone;
+
+    @Override
+    public String toString() {
+        return String.format(
+                "Customer[id=%d, firstName='%s', lastName='%s']",
+                id, firstName, lastName);
+    }
+}
