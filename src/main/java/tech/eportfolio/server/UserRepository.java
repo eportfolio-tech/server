cab63e81e51124d8e0a13e3188b0baa9f053b945
Haswell <shuyangf@student.unimelb.edu.au>
2020-08-26 03:35:39 +1000

    feat: Add UserRepository

---
+++ src/main/java/tech/eportfolio/server/UserRepository.java
@@ -0,0 +1,9 @@
+package tech.eportfolio.server;
+
+import java.util.List;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Long>{
+    List<User> findByLastName(String lastName);
+    User findById(long id);
+}
