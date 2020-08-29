cc571da982d59a14c0e4e65599d64d11c7a53e22
Haswell <shuyangf@student.unimelb.edu.au>
2020-08-29 22:30:47 +1000

    ci: add docker-compose file

--- docker_build_push.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cat token.txt | docker login https://docker.pkg.github.com -u haswf --password-stdin
-docker build --build-arg JAR_FILE=build/libs/*.jar -t docker.pkg.github.com/haswf/comp30022backenddev/eportfolio-server-docker .
-docker push docker.pkg.github.com/haswf/comp30022backenddev/eportfolio-server-docker:latest
