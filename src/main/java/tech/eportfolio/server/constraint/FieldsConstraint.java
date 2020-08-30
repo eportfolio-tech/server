5246d83e29ca7076e04d5f1469b05b42ab04d9b1
Jiachen Zhou <763056925@qq.com>
2020-08-31 03:23:53 +1000

    feat: add email and password verification

---
+++ src/main/java/tech/eportfolio/server/constraint/FieldsConstraint.java
@@ -0,0 +1,34 @@
+package tech.eportfolio.server.constraint;
+
+import validator.FieldsValidator;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+
+@Constraint(validatedBy = FieldsValidator.class)
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface FieldsConstraint {
+
+    String message() default "Fields values don't match!";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+
+    String field();
+
+    String fieldMatch();
+
+    @Target({ElementType.TYPE})
+    @Retention(RetentionPolicy.RUNTIME)
+    @interface List {
+        FieldsConstraint[] value();
+    }
+
+}
--- src/main/java/tech/eportfolio/server/dto/UserDTO.java
+++ src/main/java/tech/eportfolio/server/dto/UserDTO.java
@@ -2,14 +2,30 @@ package tech.eportfolio.server.dto;
 
 import lombok.Data;
 import tech.eportfolio.server.constraint.EmailConstraint;
+import tech.eportfolio.server.constraint.FieldsConstraint;
 
+
+@FieldsConstraint.List({
+        @FieldsConstraint(
+                field = "password",
+                fieldMatch = "password_verification",
+                message = "Passwords do not match!"
+        ),
+        @FieldsConstraint(
+                field = "email",
+                fieldMatch = "email_verification",
+                message = "Email addresses do not match!"
+        )
+})
 @Data
 public class UserDTO {
     private String firstName;
     private String lastName;
     @EmailConstraint
     private String email;
+    private String email_verification;
     private String password;
+    private String password_verification;
     private String title;
     private String phone;
     private String username;
