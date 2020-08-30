8cf5c77eead3d377641691b8d83bb522e9ce84b4
Jiachen Zhou <763056925@qq.com>
2020-08-30 23:33:20 +1000

    feat: format & add target annotation

--- src/main/java/tech/eportfolio/server/constraint/EmailConstraint.java
+++ src/main/java/tech/eportfolio/server/constraint/EmailConstraint.java
@@ -5,13 +5,11 @@ import validator.EmailValidator;
 
 import javax.validation.Constraint;
 import javax.validation.Payload;
-import java.lang.annotation.Documented;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.*;
 
 @Documented
 @Constraint(validatedBy = EmailValidator.class)
-//@Target( { ElementType.METHOD, ElementType.FIELD })
+@Target({ElementType.METHOD, ElementType.FIELD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface EmailConstraint {
     String message() default "Invalid email address";
--- src/main/java/validator/EmailValidator.java
+++ src/main/java/validator/EmailValidator.java
@@ -5,21 +5,21 @@ import tech.eportfolio.server.constraint.EmailConstraint;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
-public class EmailValidator implements
-        ConstraintValidator<EmailConstraint, String> {
+public class EmailValidator implements ConstraintValidator<EmailConstraint, String> {
 
     @Override
     public void initialize(EmailConstraint contactNumber) {
+
     }
 
     @Override
-    public boolean isValid(String contactField,
-                           ConstraintValidatorContext cxt) {
-        return contactField != null && contactField.matches("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+" +
-                "(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]" +
-                "|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+" +
-                "[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.)" +
-                "{3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])");
+    public boolean isValid(String contactField, ConstraintValidatorContext context) {
+        return contactField != null && contactField.matches("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\." +
+                "[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]" +
+                "|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9]" +
+                "(?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]" +
+                "|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]" +
+                "|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])");
     }
 
 }
