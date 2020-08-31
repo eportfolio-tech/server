bd649c5cacc3a84b2e3acee3927090d5a06d0122
Jiachen Zhou <763056925@qq.com>
2020-08-31 19:23:36 +1000

    delete email not valid exception

--- src/main/java/tech/eportfolio/server/exception/EmailNotValidException.java
+++
@@ -1,7 +0,0 @@
-package tech.eportfolio.server.exception;
-
-public class EmailNotValidException extends RuntimeException {
-    public EmailNotValidException(String email) {
-        super("User registration failed, email " + email + " is not valid");
-    }
-}
