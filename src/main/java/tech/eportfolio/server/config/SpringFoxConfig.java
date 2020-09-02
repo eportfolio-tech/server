a7150ea88e016c0c502fec8986578d6325d8fe91
Haswell <shuyangf@student.unimelb.edu.au>
2020-09-02 23:45:17 +1000

    docs(swagger): Config Swagger to only scan controllers of the project
    Remove entires for basic-error-controller

--- src/main/java/tech/eportfolio/server/config/SpringFoxConfig.java
+++ src/main/java/tech/eportfolio/server/config/SpringFoxConfig.java
@@ -15,7 +15,9 @@ public class SpringFoxConfig {
     public Docket apiDocket() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                // scan only the package of your project
+                // See https://stackoverflow.com/questions/32941917/remove-basic-error-controller-in-springfox-swaggerui
+                .apis(RequestHandlerSelectors.basePackage("tech.eportfolio.server"))
                 .paths(PathSelectors.any())
                 .build();
     }
