09a0dd5059660df061cf0c7e247f8591b749c4c7
Jiachen Zhou <763056925@qq.com>
2020-09-08 05:13:58 +1000

    feat: add reset-pasword reset and verify api

--- src/main/java/tech/eportfolio/server/controller/UserController.java
+++ src/main/java/tech/eportfolio/server/controller/UserController.java
@@ -5,24 +5,22 @@ import io.swagger.annotations.Authorization;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import tech.eportfolio.server.constant.SecurityConstant;
-import tech.eportfolio.server.dto.PasswordResetRequestBody;
 import tech.eportfolio.server.exception.UserNotFoundException;
 import tech.eportfolio.server.model.Tag;
 import tech.eportfolio.server.model.User;
+import tech.eportfolio.server.model.UserPrincipal;
 import tech.eportfolio.server.model.UserTag;
 import tech.eportfolio.server.repository.UserRepository;
 import tech.eportfolio.server.service.UserService;
 import tech.eportfolio.server.service.UserTagService;
+import tech.eportfolio.server.utility.JWTTokenProvider;
 
-import javax.validation.Valid;
-import javax.validation.constraints.Null;
-import java.nio.file.AccessDeniedException;
 import java.util.List;
-import java.util.Optional;
 
 /**
  * @author Haswell
@@ -38,11 +36,14 @@ public class UserController {
 
     private final UserTagService userTagService;
 
+    private final JWTTokenProvider jwtTokenProvider;
+
     @Autowired
-    public UserController(UserService service, UserRepository repository, UserTagService userTagService) {
+    public UserController(UserService service, UserRepository repository, UserTagService userTagService, JWTTokenProvider jwtTokenProvider) {
         this.userService = service;
         this.repository = repository;
         this.userTagService = userTagService;
+        this.jwtTokenProvider = jwtTokenProvider;
     }
 
     /**
@@ -54,11 +55,7 @@ public class UserController {
     @GetMapping("/{username}")
     @ApiOperation(value = "", authorizations = {@Authorization(value = "JWT")})
     public User findOneUser(@PathVariable String username) {
-        Optional<User> user = userService.findByUsername(username);
-        if (user.isEmpty()) {
-            throw new UserNotFoundException(username);
-        }
-        return user.get();
+        return userService.findByUsername(username).orElseThrow(() -> (new UserNotFoundException(username)));
     }
 
     /**
@@ -70,18 +67,11 @@ public class UserController {
     @ResponseStatus(HttpStatus.ACCEPTED)
     @PostMapping("/{username}/password-reset")
     @ApiOperation(value = "", authorizations = {@Authorization(value = "JWT")})
-    public ResponseEntity<Null> passwordReset(@PathVariable String username, @RequestBody @Valid PasswordResetRequestBody passwordResetRequestBody) throws AccessDeniedException {
-        Optional<User> userOptional = userService.findByUsername(username);
-        if (userOptional.isEmpty()) {
-            throw new UserNotFoundException(username);
-        }
-        User user = userOptional.get();
-        if (userService.verifyPassword(user, passwordResetRequestBody.getOldPassword())) {
-            userService.changePassword(user, passwordResetRequestBody.getNewPassword());
-            return new ResponseEntity<>(HttpStatus.ACCEPTED);
-        } else {
-            throw new AccessDeniedException(SecurityConstant.ACCESS_DENIED_MESSAGE);
-        }
+    public ResponseEntity<User> passwordReset(@PathVariable String username) {
+        User user = userService.findByUsername(username).orElseThrow(() -> (new UserNotFoundException(username)));
+        UserPrincipal userPrincipal = new UserPrincipal(user);
+        HttpHeaders jwtHeader = getJwtHeader(userPrincipal);
+        return new ResponseEntity<>(user, jwtHeader, HttpStatus.OK);
     }
 
     @PutMapping("/{username}")
@@ -130,5 +120,11 @@ public class UserController {
     public List<UserTag> deleteUserTags(@PathVariable String username, @RequestBody List<Tag> tags) {
         User user = userService.findByUsername(username).orElseThrow(() -> (new UserNotFoundException(username)));
         return userTagService.delete(user, tags);
+    }
+
+    private HttpHeaders getJwtHeader(UserPrincipal userPrincipal) {
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add(SecurityConstant.JWT_TOKEN_HEADER, jwtTokenProvider.generateJWTToken(userPrincipal, SecurityConstant.SECRET));
+        return httpHeaders;
     }
 }
