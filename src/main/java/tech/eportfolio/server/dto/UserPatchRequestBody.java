a1d4ce567bf1b5d9ddbc3eaf0e941d7aed4316f4
Haswell <shuyangf@student.unimelb.edu.au>
2020-09-16 15:16:39 +1000

    feat(User): Change UserPatchRequestBody to allow modification to user avatar

--- src/main/java/tech/eportfolio/server/dto/UserPatchRequestBody.java
+++ src/main/java/tech/eportfolio/server/dto/UserPatchRequestBody.java
@@ -5,17 +5,23 @@ import lombok.Data;
 import net.andreinc.mockneat.MockNeat;
 import tech.eportfolio.server.common.constraint.ValidEmail;
 
+import javax.validation.constraints.Size;
+
 @Data
 @Builder
 public class UserPatchRequestBody {
+    @Size(min = 1)
     private String firstName;
-
+    @Size(min = 1)
     private String lastName;
     @ValidEmail
     private String email;
-
+    @Size(min = 1)
     private String title;
+    @Size(min = 1)
     private String phone;
+    @Size(min = 10)
+    private String avatarUrl;
 
     public static UserPatchRequestBody mock() {
         MockNeat mock = MockNeat.threadLocal();
