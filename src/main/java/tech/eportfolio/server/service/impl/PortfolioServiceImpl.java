138af37e640a468cb6e54be016d4fb8a61b97422
Jiachen Zhou <763056925@qq.com>
2020-09-17 20:29:55 +1000

    fix(fix pagination problem):

--- src/main/java/tech/eportfolio/server/service/impl/PortfolioServiceImpl.java
+++ src/main/java/tech/eportfolio/server/service/impl/PortfolioServiceImpl.java
@@ -6,12 +6,12 @@ import ma.glasnost.orika.impl.DefaultMapperFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.core.query.TextCriteria;
 import org.springframework.data.mongodb.core.query.TextQuery;
+import org.springframework.data.repository.support.PageableExecutionUtils;
 import org.springframework.stereotype.Service;
 import tech.eportfolio.server.dto.PortfolioDTO;
 import tech.eportfolio.server.model.Portfolio;
@@ -90,8 +90,10 @@ public class PortfolioServiceImpl implements PortfolioService {
     public Page<Portfolio> searchWithPagination(String text, Pageable pageable) {
         Query query = TextQuery.queryText(new TextCriteria().matchingAny(text)).sortByScore().with(pageable);
         List<Portfolio> result = mongoTemplate.find(query, Portfolio.class);
-        long count = mongoTemplate.count(query, Portfolio.class);
-        return new PageImpl<>(result, pageable, count);
+        return PageableExecutionUtils.getPage(
+                result,
+                pageable,
+                () -> mongoTemplate.count(Query.of(query).limit(-1).skip(-1), Portfolio.class));
     }
 
     @Override
