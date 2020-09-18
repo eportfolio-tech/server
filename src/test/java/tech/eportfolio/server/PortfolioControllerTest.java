d5e934998d7f38951bf52426c422fd2ac189264d
Haswell <shuyangf@student.unimelb.edu.au>
2020-09-19 05:22:53 +1000

    test(search): Add test case to verify search result matches a user's role

--- src/test/java/tech/eportfolio/server/PortfolioControllerTest.java
+++ src/test/java/tech/eportfolio/server/PortfolioControllerTest.java
@@ -12,16 +12,25 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithAnonymousUser;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import tech.eportfolio.server.common.constant.Role;
+import tech.eportfolio.server.common.constant.Visibility;
 import tech.eportfolio.server.dto.PortfolioDTO;
 import tech.eportfolio.server.dto.UserDTO;
+import tech.eportfolio.server.model.Portfolio;
 import tech.eportfolio.server.model.User;
 import tech.eportfolio.server.service.PortfolioService;
 import tech.eportfolio.server.service.UserService;
+import tech.eportfolio.server.service.VerificationService;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
 
 import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -47,8 +56,12 @@ public class PortfolioControllerTest {
     @Autowired
     private PortfolioService portfolioService;
 
+    @Autowired
+    VerificationService verificationService;
+
     private User testUser;
-    private User user2;
+    private User secondUser;
+    private User thirdUser;
 
     private PortfolioDTO portfolioDTO;
     private int count = 0;
@@ -59,31 +72,28 @@ public class PortfolioControllerTest {
         UserDTO userDTO = UserDTO.mock();
         userDTO.setUsername("test");
 
+        // Verify testUser
         testUser = userService.register(userService.fromUserDTO(userDTO));
-        user2 = userService.register(userService.fromUserDTO(UserDTO.mock()));
+        testUser = verificationService.verify(testUser);
+
+        secondUser = userService.register(userService.fromUserDTO(UserDTO.mock()));
+        thirdUser = userService.register(userService.fromUserDTO(UserDTO.mock()));
 
         portfolioDTO = PortfolioDTO.mock();
         portfolioService.create(testUser, portfolioService.fromPortfolioDTO(portfolioDTO));
         count++;
-        portfolioService.create(user2, portfolioService.fromPortfolioDTO(portfolioDTO));
+
+        portfolioDTO.setVisibility(Visibility.VERIFIED_USER);
+        portfolioService.create(secondUser, portfolioService.fromPortfolioDTO(portfolioDTO));
+        count++;
+
+        portfolioDTO.setVisibility(Visibility.UNVERIFIED_USER);
+        portfolioService.create(thirdUser, portfolioService.fromPortfolioDTO(portfolioDTO));
         count++;
     }
 
-//    @Test
-//    public void ifFoundSomethingThenReturn200AndExpectNotEmpty() throws Exception {
-//        String query = portfolioDTO.getDescription();
-//        this.mockMvc.perform(get("/portfolio/search")
-//                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON)
-//                .param("query", query)
-//                .param("page", "0")
-//                .param("size", "10")
-//        ).andDo(print())
-//                .andExpect(status().isOk())
-//                .andExpect(jsonPath("$.status").value("success"))
-//                .andExpect(jsonPath("$.data.content").isNotEmpty());
-//    }
-
     @Test
+    @WithMockUser(username = "test")
     public void ifFoundNothingThenReturn200AndExpectEmpty() throws Exception {
         String query = RandomStringUtils.randomAlphabetic(8);
         this.mockMvc.perform(get("/portfolio/search")
@@ -111,6 +121,7 @@ public class PortfolioControllerTest {
 
 
     @Test
+    @WithMockUser(username = "test")
     public void ifSizeIsLargeThanResultCountThenReturnLastPage() throws Exception {
         String query = portfolioDTO.getTitle();
         int size = 5;
@@ -125,10 +136,11 @@ public class PortfolioControllerTest {
                 .andExpect(jsonPath("$.data.content", hasSize(count)))
                 .andExpect(jsonPath("$.data.numberOfElements").value(String.valueOf(count)))
                 .andExpect(jsonPath("$.data.totalElements").value(count))
-                .andExpect(jsonPath("$.data.last").value("true"));
+                .andExpect(jsonPath("$.data.last").value(String.valueOf(size > count)));
     }
 
     @Test
+    @WithMockUser(username = "test")
     public void ifSizeIsSmallerThanResultCountThenReturnFirstPage() throws Exception {
         String query = portfolioDTO.getTitle();
         int size = 1;
@@ -143,13 +155,14 @@ public class PortfolioControllerTest {
                 .andExpect(jsonPath("$.data.content", hasSize(size)))
                 .andExpect(jsonPath("$.data.numberOfElements").value(String.valueOf(size)))
                 .andExpect(jsonPath("$.data.totalElements").value(count))
-                .andExpect(jsonPath("$.data.last").value("false"));
+                .andExpect(jsonPath("$.data.last").value(String.valueOf(size > count)));
     }
 
     @Test
+    @WithMockUser(username = "test")
     public void ifSizeEqualsResultCountThenReturnLastPage() throws Exception {
         String query = portfolioDTO.getTitle();
-        int size = 2;
+        int size = count;
         this.mockMvc.perform(get("/portfolio/search")
                 .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON)
                 .param("query", query)
@@ -161,8 +174,71 @@ public class PortfolioControllerTest {
                 .andExpect(jsonPath("$.data.content", hasSize(size)))
                 .andExpect(jsonPath("$.data.numberOfElements").value(String.valueOf(size)))
                 .andExpect(jsonPath("$.data.totalElements").value(count))
-                .andExpect(jsonPath("$.data.last").value("true"));
+                .andExpect(jsonPath("$.data.last").value(String.valueOf(size == count)));
 
+    }
+
+    @Test
+    @WithAnonymousUser
+    public void ifNotLoginThenSearchShouldOnlySeePublicPortfolio() throws Exception {
+        // Expect 1 public, 1 private portfolio in the database
+        String query = portfolioDTO.getTitle();
+        List<Portfolio> publicResult = portfolioService.searchWithVisibilities(query, Collections.singletonList(Visibility.PUBLIC));
+        int size = 5;
+        this.mockMvc.perform(get("/portfolio/search")
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON)
+                .param("query", query)
+                .param("page", "0")
+                .param("size", String.valueOf(size))
+        ).andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.status").value("success"))
+                .andExpect(jsonPath("$.data.numberOfElements").value(String.valueOf(publicResult.size())));
+    }
+
+    @Test
+    @WithMockUser("test")
+    public void ifUnverifiedThenSearchShouldOnlySeePublicAndUnverifiedPortfolio() throws Exception {
+        // Update test user's role to unverified
+        testUser.setRoles(Role.ROLE_UNVERIFIED_USER.name());
+        userService.save(testUser);
+
+        String query = portfolioDTO.getTitle();
+        List<Visibility> searchVisibility = new LinkedList<>();
+        searchVisibility.add(Visibility.PUBLIC);
+        searchVisibility.add(Visibility.UNVERIFIED_USER);
+        List<Portfolio> searchResult = portfolioService.searchWithVisibilities(query, searchVisibility);
+        int size = 5;
+        this.mockMvc.perform(get("/portfolio/search")
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON)
+                .param("query", query)
+                .param("page", "0")
+                .param("size", String.valueOf(size))
+        ).andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.status").value("success"))
+                .andExpect(jsonPath("$.data.numberOfElements").value(String.valueOf(searchResult.size())));
+    }
+
+    @Test
+    @WithMockUser("test")
+    public void ifVerifiedThenSearchShouldSeeAllExceptPrivatePortfolio() throws Exception {
+        String query = portfolioDTO.getTitle();
+        List<Visibility> searchVisibility = new LinkedList<>();
+        searchVisibility.add(Visibility.PUBLIC);
+        searchVisibility.add(Visibility.UNVERIFIED_USER);
+        searchVisibility.add(Visibility.VERIFIED_USER);
+        List<Portfolio> searchResult = portfolioService.searchWithVisibilities(query, searchVisibility);
+        int size = 5;
+        this.mockMvc.perform(get("/portfolio/search")
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON)
+                .param("query", query)
+                .param("page", "0")
+                .param("size", String.valueOf(size))
+        ).andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.status").value("success"))
+                .andExpect(jsonPath("$.data.numberOfElements").value(String.valueOf(searchResult.size())));
     }
 
 
