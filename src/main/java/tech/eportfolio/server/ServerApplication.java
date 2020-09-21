43ec53c6fd54c0f3d8ce134aaab64e2dec937bb4
Haswell <shuyangf@student.unimelb.edu.au>
2020-09-21 19:33:56 +1000

    fix: Change mail.debug to false

--- src/main/java/tech/eportfolio/server/ServerApplication.java
+++ src/main/java/tech/eportfolio/server/ServerApplication.java
@@ -47,7 +47,7 @@ public class ServerApplication {
         props.put("mail.transport.protocol", "smtp");
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.debug", "trAue");
+        props.put("mail.debug", "false");
         props.put("mail.smtp.socketFactory.port", "465");
         props.put("mail.smtp.starttls.required", "true");
         props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
