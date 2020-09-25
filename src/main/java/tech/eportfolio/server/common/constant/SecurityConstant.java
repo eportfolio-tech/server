e44ccc531bd44d02ec4a7b2a00d8cfc74ffa371b
Haswell <shuyangf@student.unimelb.edu.au>
2020-09-25 18:54:39 +1000

    fix(Auth): Change access token validity period to 15 minutes

--- src/main/java/tech/eportfolio/server/common/constant/SecurityConstant.java
+++ src/main/java/tech/eportfolio/server/common/constant/SecurityConstant.java
@@ -29,7 +29,8 @@ public class SecurityConstant {
     public static final String[] POST_ONLY = {"/blobs/image/**", "/verification/verify/**"};
     public static final String[] GET_ONLY = {"/tags/", "/portfolios/search/**", "/portfolios/{username}"};
     public static final Period REFRESH_TOKEN_VALIDITY = Period.weeks(2);
-    public static final Period ACCESS_TOKEN_VALIDITY = Period.seconds(5);
+    //
+    public static final Period ACCESS_TOKEN_VALIDITY = Period.minutes(15);
     public static final Period PASSWORD_RECOVERY_TOKEN_VALIDITY = Period.minutes(15);
     public static final Period EMAIL_VERIFICATION_TOKEN_VALIDITY = Period.hours(12);
 
