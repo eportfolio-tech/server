a0235fc96a095b8019f0509f147b2eae5aeaf835
Haswell <shuyangf@student.unimelb.edu.au>
2020-09-25 17:16:25 +1000

    fix: Remove duplicate code

--- src/main/java/tech/eportfolio/server/common/utility/ParagraphProvider.java
+++ src/main/java/tech/eportfolio/server/common/utility/ParagraphProvider.java
@@ -8,7 +8,12 @@ import java.util.Random;
 
 public class ParagraphProvider {
 
-    public static String paragraph() {
+
+    private ParagraphProvider() {
+
+    }
+
+    private static List<String> paragraphs() {
         List<String> paragraph = new ArrayList<>();
         paragraph.add(Faker.instance().buffy().quotes());
         paragraph.add(Faker.instance().backToTheFuture().quote());
@@ -35,36 +40,15 @@ public class ParagraphProvider {
         paragraph.add(Faker.instance().twinPeaks().quote());
         paragraph.add(Faker.instance().witcher().quote());
         paragraph.add(Faker.instance().yoda().quote());
-        return String.join(" ", paragraph);
+        return paragraph;
+    }
+
+    public static String paragraph() {
+        return String.join(" ", paragraphs());
     }
 
     public static String sentence() {
-        List<String> paragraph = new ArrayList<>();
-        paragraph.add(Faker.instance().buffy().quotes());
-        paragraph.add(Faker.instance().backToTheFuture().quote());
-        paragraph.add(Faker.instance().dune().quote());
-        paragraph.add(Faker.instance().elderScrolls().quote());
-        paragraph.add(Faker.instance().friends().quote());
-        paragraph.add(Faker.instance().gameOfThrones().quote());
-        paragraph.add(Faker.instance().harryPotter().quote());
-        paragraph.add(Faker.instance().hitchhikersGuideToTheGalaxy().quote());
-        paragraph.add(Faker.instance().hitchhikersGuideToTheGalaxy().marvinQuote());
-        paragraph.add(Faker.instance().hobbit().quote());
-        paragraph.add(Faker.instance().howIMetYourMother().quote());
-        paragraph.add(Faker.instance().leagueOfLegends().quote());
-        paragraph.add(Faker.instance().lebowski().quote());
-        paragraph.add(Faker.instance().matz().quote());
-        paragraph.add(Faker.instance().overwatch().quote());
-        paragraph.add(Faker.instance().princessBride().quote());
-        paragraph.add(Faker.instance().rickAndMorty().quote());
-        paragraph.add(Faker.instance().robin().quote());
-        paragraph.add(Faker.instance().shakespeare().hamletQuote());
-        paragraph.add(Faker.instance().shakespeare().asYouLikeItQuote());
-        paragraph.add(Faker.instance().shakespeare().kingRichardIIIQuote());
-        paragraph.add(Faker.instance().shakespeare().romeoAndJulietQuote());
-        paragraph.add(Faker.instance().twinPeaks().quote());
-        paragraph.add(Faker.instance().witcher().quote());
-        paragraph.add(Faker.instance().yoda().quote());
-        return paragraph.get(new Random().nextInt(paragraph.size()));
+        List<String> sentences = paragraphs();
+        return sentences.get(new Random().nextInt(sentences.size()));
     }
 }
