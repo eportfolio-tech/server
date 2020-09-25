1966c73b52b1c15afe5f2049bb73ede09d29966f
Jiachen Zhou <763056925@qq.com>
2020-09-26 00:19:12 +1000

    feat: add Mock() to TemplateDTO

--- src/main/java/tech/eportfolio/server/dto/TemplateDTO.java
+++ src/main/java/tech/eportfolio/server/dto/TemplateDTO.java
@@ -1,9 +1,13 @@
 package tech.eportfolio.server.dto;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.github.javafaker.Faker;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import org.apache.commons.lang3.RandomStringUtils;
 
 import javax.validation.constraints.NotBlank;
 
@@ -19,4 +23,15 @@ public class TemplateDTO {
 
     @NotBlank
     private String description;
+
+    public static TemplateDTO mock() {
+        String title = Faker.instance().name().title();
+        ObjectMapper mapper = new ObjectMapper();
+        ObjectNode objectNode = mapper.createObjectNode();
+        JsonNode boilerplate = objectNode.put(RandomStringUtils.randomAlphabetic(4), RandomStringUtils.randomAlphanumeric(8));
+        String description = Faker.instance().gameOfThrones().quote();
+
+        return TemplateDTO.builder().title(title).boilerplate(boilerplate).description(description).build();
+    }
+
 }
