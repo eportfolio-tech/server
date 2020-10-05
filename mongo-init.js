05daf854f76aa505094d1cffb66e5fd1c8e556f8
Haswell <shuyangf@student.unimelb.edu.au>
2020-10-05 12:57:34 +0200

    Add mongoDB to docker-compose

---
+++ mongo-init.js
@@ -0,0 +1,12 @@
+db.createUser(
+    {
+        user:  "comp30022",
+        pwd: "6iqZ1ViYak0I",
+        roles: [
+            {
+                role: "readWrite",
+                db: "eportfolio"
+            }
+        ]
+    }
+);
