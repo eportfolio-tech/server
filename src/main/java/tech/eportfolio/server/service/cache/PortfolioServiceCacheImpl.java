4b2825d4b4ca3d39bc2983528139d936bdbe8f99
FishWith7sMemory <763056925@qq.com>
2020-10-11 19:27:49 +1100

    feat(cache): add cache to endpoints in portfolio service

---
+++ src/main/java/tech/eportfolio/server/service/cache/PortfolioServiceCacheImpl.java
@@ -0,0 +1,109 @@
+package tech.eportfolio.server.service.cache;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.stereotype.Service;
+import tech.eportfolio.server.common.constant.Visibility;
+import tech.eportfolio.server.dto.PortfolioDTO;
+import tech.eportfolio.server.model.Portfolio;
+import tech.eportfolio.server.model.User;
+import tech.eportfolio.server.service.PortfolioService;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+@Service
+@CacheConfig(cacheNames = "portfolios")
+@Qualifier("PortfolioServiceCacheImpl")
+public class PortfolioServiceCacheImpl implements PortfolioService {
+
+    private final PortfolioService portfolioService;
+
+    @Autowired
+    public PortfolioServiceCacheImpl(@Qualifier("portfolioServiceImpl") PortfolioService portfolioService) {
+        this.portfolioService = portfolioService;
+    }
+
+    @Override
+    @Cacheable
+    public Optional<Portfolio> findByUsername(String username) {
+        return portfolioService.findByUsername(username);
+    }
+
+    @Override
+    public List<Portfolio> findByUserIdIn(List<String> userIds) {
+        return portfolioService.findByUserIdIn(userIds);
+    }
+
+    @Override
+    public Portfolio fromPortfolioDTO(PortfolioDTO portfolioDTO) {
+        return portfolioService.fromPortfolioDTO(portfolioDTO);
+    }
+
+    @Override
+    @CachePut
+    public Portfolio create(User user, Portfolio portfolio) {
+        return portfolioService.create(user, portfolio);
+    }
+
+    @Override
+    @Cacheable
+    public Page<Portfolio> searchByKeywordWithPaginationAndVisibilities(String text, Pageable pageable, List<Visibility> visibilities) {
+        return portfolioService.searchByKeywordWithPaginationAndVisibilities(text, pageable, visibilities);
+    }
+
+    @Override
+    @Cacheable
+    public Page<Portfolio> searchByTagWithPaginationAndVisibilities(Pageable pageable, List<Visibility> visibilities, List<String> userIds) {
+        return portfolioService.searchByTagWithPaginationAndVisibilities(pageable, visibilities, userIds);
+    }
+
+    @Override
+    public Page<Portfolio> searchWithPagination(Query query, Pageable pageable) {
+        return portfolioService.searchWithPagination(query, pageable);
+    }
+
+
+    @Override
+    public List<Portfolio> searchWithVisibilities(String text, List<Visibility> visibilities) {
+        return portfolioService.searchWithVisibilities(text, visibilities);
+    }
+
+    @Override
+    public List<Portfolio> searchWithVisibilities(List<Visibility> visibilities) {
+        return portfolioService.searchWithVisibilities(visibilities);
+    }
+
+    @Override
+    @CachePut
+    public Portfolio updateContent(Portfolio portfolio, Map<String, Object> map) {
+        return portfolioService.updateContent(portfolio, map);
+    }
+
+    @Override
+    @CacheEvict
+    public Portfolio deleteContent(Portfolio portfolio) {
+        return portfolioService.deleteContent(portfolio);
+    }
+
+    @Override
+    @CachePut
+    public Portfolio updatePortfolio(Portfolio portfolio, PortfolioDTO portfolioDTO) {
+        return portfolioService.updatePortfolio(portfolio, portfolioDTO);
+    }
+
+    @Override
+    public Portfolio save(Portfolio portfolio) {
+        return portfolioService.save(portfolio);
+    }
+
+
+}
