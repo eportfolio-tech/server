7f9b788ce39d6c5a3568c471ed5f78ae614f72c9
FishWith7sMemory <763056925@qq.com>
2020-10-16 18:07:06 +1100

    refactor: leave only one bean for serialization

--- src/main/java/tech/eportfolio/server/config/RedisConfig.java
+++ src/main/java/tech/eportfolio/server/config/RedisConfig.java
@@ -5,13 +5,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.cache.RedisCacheManagerBuilderCustomizer;
-import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
-import org.springframework.data.redis.cache.RedisCacheManager;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
@@ -42,22 +39,8 @@ public class RedisConfig {
         if (StringUtils.isNotBlank(password)) {
             redisStandaloneConfiguration.setPassword(password);
         }
-        logger.info("Connect to Reids host {} on port {}", redisHostName, redisPort);
+        logger.info("Connect to Redis host {} on port {}", redisHostName, redisPort);
         return new JedisConnectionFactory(redisStandaloneConfiguration);
-    }
-
-    @Bean
-    public CacheManager redisCacheManager(RedisConnectionFactory redisConnectionFactory) {
-        RedisSerializationContext.SerializationPair<Object> jsonSerializer =
-                RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer());
-        return RedisCacheManager.RedisCacheManagerBuilder
-                .fromConnectionFactory(redisConnectionFactory)
-                .cacheDefaults(
-                        RedisCacheConfiguration.defaultCacheConfig()
-                                .entryTtl(Duration.ofDays(1))
-                                .serializeValuesWith(jsonSerializer)
-                )
-                .build();
     }
 
     @Bean
