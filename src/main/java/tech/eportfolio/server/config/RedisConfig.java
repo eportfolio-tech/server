ec321c2016227ca78d3b3ec4c627969ef4a9c1dd
Haswell <shuyangf@student.unimelb.edu.au>
2020-10-16 11:32:53 +1100

    fix: Fix deserialization bug in Redis Cache when running locally

--- src/main/java/tech/eportfolio/server/config/RedisConfig.java
+++ src/main/java/tech/eportfolio/server/config/RedisConfig.java
@@ -5,10 +5,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.cache.RedisCacheManagerBuilderCustomizer;
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
@@ -33,28 +36,39 @@ public class RedisConfig {
     @Value("${spring.redis.password}")
     private String password;
 
-
     @Bean
     JedisConnectionFactory jedisConnectionFactory() {
         RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration(redisHostName, redisPort);
         if (StringUtils.isNotBlank(password)) {
             redisStandaloneConfiguration.setPassword(password);
         }
-        logger.info("redis host: " + redisStandaloneConfiguration.getHostName());
+        logger.info("Connect to Reids host {} on port {}", redisHostName, redisPort);
         return new JedisConnectionFactory(redisStandaloneConfiguration);
+    }
+
+    @Bean
+    public CacheManager redisCacheManager(RedisConnectionFactory redisConnectionFactory) {
+        RedisSerializationContext.SerializationPair<Object> jsonSerializer =
+                RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer());
+        return RedisCacheManager.RedisCacheManagerBuilder
+                .fromConnectionFactory(redisConnectionFactory)
+                .cacheDefaults(
+                        RedisCacheConfiguration.defaultCacheConfig()
+                                .entryTtl(Duration.ofDays(1))
+                                .serializeValuesWith(jsonSerializer)
+                )
+                .build();
     }
 
     @Bean
     RedisCacheManagerBuilderCustomizer redisCacheManagerBuilderCustomizer() {
         RedisSerializationContext.SerializationPair<Object> jsonSerializer =
                 RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer());
-        return (builder) -> {
+        return builder -> {
             Map<String, RedisCacheConfiguration> configurationMap = new HashMap<>();
             configurationMap.put("users", RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofHours(2)).serializeValuesWith(jsonSerializer));
             configurationMap.put("portfolios", RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofHours(1)).serializeValuesWith(jsonSerializer));
             builder.withInitialCacheConfigurations(configurationMap);
         };
     }
-
-
 }
