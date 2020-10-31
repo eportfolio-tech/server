d3af10f7e28337e1aee943a5940b2012fca556c2
Haswell <shuyangf@student.unimelb.edu.au>
2020-11-01 10:30:13 +1100

    fix: Read MongoDB connection string from environment variables

---
+++ src/main/java/tech/eportfolio/server/config/MongoConfig.java
@@ -0,0 +1,35 @@
+package tech.eportfolio.server.config;
+
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+@Configuration
+public class MongoConfig {
+
+    @Value("${spring.data.mongodb.uri}")
+    String mongoConnectionString;
+
+    @Value("${spring.data.mongodb.database}")
+    String database;
+
+    @Bean
+    public MongoClient mongo() {
+        ConnectionString connectionString = new ConnectionString(mongoConnectionString);
+        MongoClientSettings mongoClientSettings = MongoClientSettings.builder()
+                .applyConnectionString(connectionString)
+                .build();
+
+        return MongoClients.create(mongoClientSettings);
+    }
+
+    @Bean
+    public MongoTemplate mongoTemplate() {
+        return new MongoTemplate(mongo(), database);
+    }
+}
